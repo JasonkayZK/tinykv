451dd1eddce5ea36273d474481d8fad73aa21412
JasonkayZK <271226192@qq.com>
2022-09-12 13:25:49 +0800

    temp save

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -4,17 +4,76 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
+	"time"
 )
 
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
-	// Your Data Here (1).
+	// Done Your Data Here (1).
+	StoreAddr     string
+	Raft          bool
+	SchedulerAddr string
+	LogLevel      string
+
+	DBPath string // Directory to store the data in. Should exist and be writable.
+
+	// raft_base_tick_interval is a base tick interval (ms).
+	RaftBaseTickInterval     time.Duration
+	RaftHeartbeatTicks       int
+	RaftElectionTimeoutTicks int
+
+	// Interval to gc unnecessary raft log (ms).
+	RaftLogGCTickInterval time.Duration
+	// When entry count exceed this value, gc will be forced trigger.
+	RaftLogGcCountLimit uint64
+
+	// Interval (ms) to check region whether you need to be split or not.
+	SplitRegionCheckTickInterval time.Duration
+	// delay time before deleting a stale peer
+	SchedulerHeartbeatTickInterval      time.Duration
+	SchedulerStoreHeartbeatTickInterval time.Duration
+
+	// When region [a,e) size meets regionMaxSize, it will be split into
+	// several regions [a,b), [b,c), [c,d), [d,e). And the size of [a,b),
+	// [b,c), [c,d) will be regionSplitSize (maybe a little larger).
+	RegionMaxSize   uint64
+	RegionSplitSize uint64
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
-	// Your Code Here (1).
-	return nil
+	// Done Your Code Here (1).
+
+	return &StandAloneStorage{
+		StoreAddr:     conf.StoreAddr,
+		Raft:          conf.Raft,
+		SchedulerAddr: conf.SchedulerAddr,
+		LogLevel:      conf.LogLevel,
+
+		DBPath: conf.DBPath, // Directory to store the data in. Should exist and be writable.
+
+		// raft_base_tick_interval is a base tick interval (ms).
+		RaftBaseTickInterval:     conf.RaftBaseTickInterval,
+		RaftHeartbeatTicks:       conf.RaftHeartbeatTicks,
+		RaftElectionTimeoutTicks: conf.RaftElectionTimeoutTicks,
+
+		// Interval to gc unnecessary raft log (ms).
+		RaftLogGCTickInterval: conf.RaftLogGCTickInterval,
+		// When entry count exceed this value, gc will be forced trigger.
+		RaftLogGcCountLimit: conf.RaftLogGcCountLimit,
+
+		// Interval (ms) to check region whether you need to be split or not.
+		SplitRegionCheckTickInterval: conf.SplitRegionCheckTickInterval,
+		// delay time before deleting a stale peer
+		SchedulerHeartbeatTickInterval:      conf.SchedulerHeartbeatTickInterval,
+		SchedulerStoreHeartbeatTickInterval: conf.SchedulerStoreHeartbeatTickInterval,
+
+		// When region [a,e) size meets regionMaxSize, it will be split into
+		// several regions [a,b), [b,c), [c,d), [d,e). And the size of [a,b),
+		// [b,c), [c,d) will be regionSplitSize (maybe a little larger).
+		RegionMaxSize:   conf.RegionMaxSize,
+		RegionSplitSize: conf.RegionSplitSize,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
